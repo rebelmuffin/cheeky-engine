63eae296049bbc258f748850bad8d0c3020051d6
rebelmuffin <43506467+rebelmuffin@users.noreply.github.com>
2024-08-17 19:01:07 +0100

    Adjust shader grabber to include compute shaders

--- shader_grabber.sh
+++ shader_grabber.sh
@@ -6,7 +6,7 @@ vert=(`find $SHADER_DIR -name "*.vert"`)
 frag=(`find $SHADER_DIR -name "*.frag"`)
 comp=(`find $SHADER_DIR -name "*.comp"`)
 
-shaders=( "${vert[@]}" "${frag[@]}")
+shaders=( "${vert[@]}" "${frag[@]}" "${comp[@]}" )
 for i in "${shaders[@]}"; do
     echo "$i";
 done
