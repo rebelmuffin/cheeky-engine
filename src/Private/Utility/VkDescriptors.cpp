9acef6251fec47f88f29df6777c0cba65770248c
rebelmuffin <43506467+rebelmuffin@users.noreply.github.com>
2025-08-28 20:13:08 +0100

    Create dynamic descriptor allocator that can be used to allocate "infinite" amounts of descriptor sets

--- src/Private/Utility/VkDescriptors.cpp
+++ src/Private/Utility/VkDescriptors.cpp
@@ -1,4 +1,8 @@
 #include "Utility/VkDescriptors.h"
+#include "VkBootstrapDispatch.h"
+#include "VkTypes.h"
+
+#include <vulkan/vulkan_core.h>
 
 namespace Utils
 {
@@ -67,6 +71,7 @@ namespace Utils
     void DescriptorAllocator::DestroyPool(vkb::DispatchTable device_dispatch)
     {
         device_dispatch.destroyDescriptorPool(m_pool, nullptr);
+        m_pool = VK_NULL_HANDLE;
     }
 
     VkDescriptorSet DescriptorAllocator::Allocate(vkb::DispatchTable device_dispatch, VkDescriptorSetLayout layout_set)
@@ -82,6 +87,109 @@ namespace Utils
         VK_CHECK(device_dispatch.allocateDescriptorSets(&info, &descriptor_set));
 
         return descriptor_set;
+    }
+
+    void DescriptorAllocatorDynamic::Init(vkb::DispatchTable device_dispatch, uint32_t initial_max_sets,
+                                          std::span<PoolSizeRatio> pool_ratios)
+    {
+        // create the first pool
+        VkDescriptorPool new_pool = AllocateNewPool(device_dispatch, initial_max_sets, pool_ratios);
+        m_ready_pools.push_back(new_pool);
+        m_size_ratios = std::vector<PoolSizeRatio>(pool_ratios.begin(), pool_ratios.end());
+    }
+
+    void DescriptorAllocatorDynamic::ClearDescriptors(vkb::DispatchTable device_dispatch)
+    {
+        for (VkDescriptorPool pool : m_ready_pools)
+        {
+            device_dispatch.resetDescriptorPool(pool, 0);
+        }
+
+        for (VkDescriptorPool pool : m_full_pools)
+        {
+            device_dispatch.resetDescriptorPool(pool, 0);
+            m_ready_pools.push_back(pool);
+        }
+        m_full_pools.clear();
+    }
+
+    void DescriptorAllocatorDynamic::DestroyPools(vkb::DispatchTable device_dispatch)
+    {
+        for (VkDescriptorPool pool : m_ready_pools)
+        {
+            device_dispatch.destroyDescriptorPool(pool, nullptr);
+        }
+        m_ready_pools.clear();
+
+        for (VkDescriptorPool pool : m_full_pools)
+        {
+            device_dispatch.destroyDescriptorPool(pool, nullptr);
+        }
+        m_full_pools.clear();
+    }
+
+    VkDescriptorSet DescriptorAllocatorDynamic::Allocate(vkb::DispatchTable device_dispatch,
+                                                         VkDescriptorSetLayout layout_set)
+    {
+        VkDescriptorPool pool = GetPool(device_dispatch);
+
+        VkDescriptorSetAllocateInfo info{};
+        info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+        info.pNext = nullptr;
+        info.descriptorPool = pool;
+        info.descriptorSetCount = 1;
+        info.pSetLayouts = &layout_set;
+
+        VkDescriptorSet descriptor_set;
+        VkResult result = device_dispatch.allocateDescriptorSets(&info, &descriptor_set);
+        if (result == VK_ERROR_OUT_OF_POOL_MEMORY)
+        {
+            m_full_pools.emplace_back(pool);
+            std::erase_if(m_ready_pools, [pool](VkDescriptorPool& p) { return p == pool; });
+            // try again
+            return Allocate(device_dispatch, layout_set);
+        }
+
+        VK_CHECK(result);
+        return descriptor_set;
+    }
+
+    VkDescriptorPool DescriptorAllocatorDynamic::GetPool(vkb::DispatchTable device_dispatch)
+    {
+        if (m_ready_pools.empty() == false) // if any ready pools, get that
+        {
+            return m_ready_pools[0];
+        }
+
+        VkDescriptorPool new_pool = AllocateNewPool(device_dispatch, m_sets_per_pool, m_size_ratios);
+        m_ready_pools.push_back(new_pool);
+        return new_pool;
+    }
+
+    VkDescriptorPool DescriptorAllocatorDynamic::AllocateNewPool(vkb::DispatchTable device_dispatch, uint32_t max_sets,
+                                                                 std::span<PoolSizeRatio> pool_ratios)
+    {
+        std::vector<VkDescriptorPoolSize> pool_sizes;
+        pool_sizes.reserve(pool_ratios.size());
+        for (PoolSizeRatio ratio : pool_ratios)
+        {
+            pool_sizes.push_back(
+                VkDescriptorPoolSize{.type = ratio.type, .descriptorCount = uint32_t(ratio.ratio * float(max_sets))});
+        }
+
+        VkDescriptorPoolCreateInfo info{};
+        info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+        info.pNext = nullptr;
+        info.maxSets = max_sets;
+        info.poolSizeCount = uint32_t(pool_sizes.size());
+        info.pPoolSizes = pool_sizes.data();
+
+        m_sets_per_pool = max_sets * 2; // next time we will double the pool size
+
+        VkDescriptorPool new_pool;
+        VK_CHECK(device_dispatch.createDescriptorPool(&info, nullptr, &new_pool));
+
+        return new_pool;
     }
 
 } // namespace Utils
--- src/Public/Utility/VkDescriptors.h
+++ src/Public/Utility/VkDescriptors.h
@@ -2,6 +2,7 @@
 
 #include "VkTypes.h"
 #include <VkBootstrapDispatch.h>
+#include <span>
 
 namespace Utils
 {
@@ -17,6 +18,7 @@ namespace Utils
         std::vector<VkDescriptorSetLayoutBinding> m_bindings;
     };
 
+    // Simple descriptor allocator that allocates from a single pool. Will crash if you run out of pool space.
     class DescriptorAllocator
     {
       public:
@@ -32,6 +34,32 @@ namespace Utils
         VkDescriptorSet Allocate(vkb::DispatchTable device_dispatch, VkDescriptorSetLayout layout_set);
 
       private:
-        VkDescriptorPool m_pool;
+        VkDescriptorPool m_pool = VK_NULL_HANDLE;
+    };
+
+    // Dynamic descriptor allocator that can grow the pool if needed.
+    class DescriptorAllocatorDynamic
+    {
+      public:
+        struct PoolSizeRatio
+        {
+            VkDescriptorType type;
+            float ratio;
+        };
+
+        void Init(vkb::DispatchTable device_dispatch, uint32_t initial_max_sets, std::span<PoolSizeRatio> pool_ratios);
+        void ClearDescriptors(vkb::DispatchTable device_dispatch);
+        void DestroyPools(vkb::DispatchTable device_dispatch);
+        VkDescriptorSet Allocate(vkb::DispatchTable device_dispatch, VkDescriptorSetLayout layout_set);
+
+      private:
+        VkDescriptorPool GetPool(vkb::DispatchTable device_dispatch);
+        VkDescriptorPool AllocateNewPool(vkb::DispatchTable device_dispatch, uint32_t max_sets,
+                                         std::span<PoolSizeRatio> pool_ratios);
+
+        std::vector<VkDescriptorPool> m_ready_pools;
+        std::vector<VkDescriptorPool> m_full_pools;
+        std::vector<PoolSizeRatio> m_size_ratios;
+        uint32_t m_sets_per_pool;
     };
 } // namespace Utils
