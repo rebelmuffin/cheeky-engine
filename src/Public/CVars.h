5e722316c4316d4e93b8437bd2e916b336172d6d
rebelmuffin <43506467+rebelmuffin@users.noreply.github.com>
2025-09-08 20:08:46 +0100

    Add a CVar for default scene path

--- src/Public/CVars.h
+++ src/Public/CVars.h
@@ -12,6 +12,7 @@ struct CVars
     float backbuffer_scale = 1.0f;
     bool use_validation_layers = true;
     bool force_immediate_uploads = false;
+    char default_scene_path[512] = "../data/resources/BarramundiFish.glb";
 
     uint32_t ReadFromFile(std::filesystem::path path)
     {
@@ -27,7 +28,7 @@ struct CVars
         while (std::getline(stream, line))
         {
             int32_t _width, _height;
-            if (std::sscanf(line.data(), "OVERRIDE_WINDOW_SIZE=%dx%d", &_width, &_height) == 2)
+            if (std::sscanf(line.data(), "OVERRIDE_WINDOW_SIZE=%dx%d;", &_width, &_height) == 2)
             {
                 width = _width;
                 height = _height;
@@ -35,26 +36,42 @@ struct CVars
                 continue;
             }
 
-            if (std::sscanf(line.data(), "BACKBUFFER_SCALE=%f", &backbuffer_scale) == 2)
+            if (std::sscanf(line.data(), "BACKBUFFER_SCALE=%f;", &backbuffer_scale) == 2)
             {
                 total_read++;
                 continue;
             }
 
-            if (std::strstr(line.data(), "USE_VALIDATION_LAYERS=false") ||
-                std::strstr(line.data(), "USE_VALIDATION_LAYERS=0"))
+            if (std::strstr(line.data(), "USE_VALIDATION_LAYERS=false;") ||
+                std::strstr(line.data(), "USE_VALIDATION_LAYERS=0;"))
             {
                 use_validation_layers = false;
                 total_read++;
                 continue;
             }
 
-            if (std::strstr(line.data(), "FORCE_IMMEDIATE_UPLOADS=true") ||
-                std::strstr(line.data(), "FORCE_IMMEDIATE_UPLOADS=1"))
+            if (std::strstr(line.data(), "FORCE_IMMEDIATE_UPLOADS=true;") ||
+                std::strstr(line.data(), "FORCE_IMMEDIATE_UPLOADS=1;"))
             {
                 force_immediate_uploads = true;
                 total_read++;
                 continue;
+            }
+
+            if (std::sscanf(line.data(), "DEFAULT_SCENE_PATH=\"%s\";", default_scene_path))
+            {
+                size_t len = strlen(default_scene_path);
+
+                // remove any trailing CVAR syntax. I really need to make a real parser
+                if (default_scene_path[len - 1] == ';')
+                {
+                    default_scene_path[len - 1] = '\0';
+                }
+                if (default_scene_path[len - 2] == '\"')
+                {
+                    default_scene_path[len - 2] = '\0';
+                }
+                total_read++;
             }
         }
 
