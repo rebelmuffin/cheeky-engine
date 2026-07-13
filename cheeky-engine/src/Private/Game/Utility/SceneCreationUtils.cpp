ab95b8255d48c34fe7a41383c52bd872e98d70eb
rebelmuffin <43506467+rebelmuffin@users.noreply.github.com>
2026-07-13 18:53:22 +0100

    Add the ability to load colliders from extras in GLTF

--- cheeky-engine/src/Private/Game/Utility/SceneCreationUtils.cpp
+++ cheeky-engine/src/Private/Game/Utility/SceneCreationUtils.cpp
@@ -7,6 +7,10 @@
 #include "Game/Nodes/MeshNode.h"
 #include "Renderer/Utility/VkLoader.h"
 
+#include "Game/ECS/Components/ColliderComponent.h"
+#include "Game/ECS/Components/PhysicsBodyComponent.h"
+#include "simdjson.h"
+
 namespace
 {
     Game::Node& CreateGameNodeFromGLTFNode(
@@ -44,7 +48,8 @@ namespace
         Game::ECS::World& world,
         Game::Node& game_node_parent,
         const Renderer::GLTFNode& gltf_node,
-        const Renderer::GLTFScene& gltf_scene
+        const Renderer::GLTFScene& gltf_scene,
+        const Renderer::GLTFExtras& extras
     )
     {
         Game::ECSNode* created_node = nullptr;
@@ -53,11 +58,12 @@ namespace
         created_node =
             &game_node_parent.CreateChild(std::make_unique<Game::ECSNode>(world, loaded_node.name));
 
+        Game::ECS::Entity entity = created_node->Entity();
         // create a mesh component if it's supposed to have a mesh
         if (loaded_node.meshIndex.has_value())
         {
             Renderer::MeshHandle mesh = gltf_scene.loaded_meshes[loaded_node.meshIndex.value()];
-            created_node->Entity().insert(
+            entity.insert(
                 [mesh](Game::ECS::MeshComponent& mesh_component)
                 {
                     mesh_component = { mesh };
@@ -65,10 +71,45 @@ namespace
             );
         }
 
+        const auto collision_type_it = extras.find("collision_type");
+        if (collision_type_it != extras.end())
+        {
+            // there's collision!
+            if (std::get<std::string>(collision_type_it->second) == "sphere")
+            {
+                float radius = std::get<double>(extras.at("collision_radius"));
+                entity.insert(
+                    [radius](Game::ECS::SphereCollider& col)
+                    {
+                        col = { radius };
+                    }
+                );
+            }
+            else if (std::get<std::string>(collision_type_it->second) == "box")
+            {
+                glm::vec3 half_extents = std::get<glm::vec3>(extras.at("collision_half_extents"));
+                entity.insert(
+                    [half_extents](Game::ECS::BoxCollider& col)
+                    {
+                        col = { half_extents };
+                    }
+                );
+            }
+
+            bool is_dynamic = false;
+            if (extras.find("body_dynamic") != extras.end())
+            {
+                is_dynamic = std::get<bool>(extras.at("body_dynamic"));
+            }
+
+            // do this at the end to initialise the body
+            std::ignore = entity.set(Game::ECS::PhysicsBodyComponent{ is_dynamic });
+        }
+
         created_node->SetLocalTransform(Game::Transform::FromMatrix(gltf_node.transform));
         for (const Renderer::GLTFNode& child : gltf_node.children)
         {
-            CreateGameNodeFromGLTFNodeECS(world, *created_node, child, gltf_scene);
+            CreateGameNodeFromGLTFNodeECS(world, *created_node, child, gltf_scene, extras);
         }
 
         return *created_node;
@@ -126,9 +167,11 @@ namespace Game::Utils
             // the root is not a valid scene node, it simply contains the real nodes, we have to look at
             // what's inside.
             Node& scene_root = node.CreateChild<Node>(file_path.filename().c_str());
-            for (const Renderer::GLTFNode& child : scene->root_node->children)
+            for (size_t i = 0; i < scene->root_node->children.size(); i++)
             {
-                CreateGameNodeFromGLTFNodeECS(world, scene_root, child, *scene);
+                const Renderer::GLTFNode& child = scene->root_node->children[i];
+                const Renderer::GLTFExtras& extras = scene->extras[i];
+                CreateGameNodeFromGLTFNodeECS(world, scene_root, child, *scene, extras);
             }
             return;
         }
